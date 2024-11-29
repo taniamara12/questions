458b6a49222f6f7e2347544a893c966c7ec18523
taniamara12 <taniamaraaparecida019@gmail.com>
2024-11-29 19:31:12 -0300

    Add files via upload

---
+++ t-nia-main/t-nia-main/Influencer questions/perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'influenciador',
+    'Qual o influenciador mais rico do brasil?',
+    'Lucas Rangel'
+)
+
+criaCartao(
+    'influenciadora',
+    'qual o influenciador mais seguido do brasil?',
+    'Neymar'
+)
+
+criaCartao(
+    'influenciador',
+    'qual o influenciador mais famoso do mundo?',
+    'MrBeast'
+)
+
+criaCartao(
+    'influenciador',
+    'qual o influenciador mais bem pago do brasil?',
+    'Neymar'
+)
